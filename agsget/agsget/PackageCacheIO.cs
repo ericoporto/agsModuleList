f2cd36dcd8b75d669cd6078588bd61af4800ae94
ericoporto <ericoporto2008@gmail.com>
2019-07-20 00:29:16 -0300

    trying to create an interface with the package index.

--- agsget/agsget/PackageCacheIO.cs
+++ agsget/agsget/PackageCacheIO.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace agsget
 {
@@ -18,8 +20,19 @@ namespace agsget
             }
         }
 
-        public static bool PackageExists(string packageName)
+        public static bool PackageOnIndex(string packageName)
         {
+            var packageIndexAsString = File.ReadAllText(BaseFiles.GetIndexFilePath());
+            JToken token = JObject.Parse(packageIndexAsString);
+
+            // I need to iterate through the array until I find a package with the id that I need.
+            // I am not doing this yet
+            string id = (string)token.SelectToken("id");
+
+            if (id.Equals(packageName))
+            {
+                return true;
+            }
 
             return false;
         }
