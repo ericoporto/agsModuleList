8a9805fc85cc62a58a5e3bbc7432a1b9828a0a71
ericoporto <ericoporto2008@gmail.com>
2020-01-27 21:34:24 +0000

    initial files for AgsGet Editor Plugin

---
+++ agsget/agsget/EditorPlugin/AGS.Plugin.AgsGet/PluginMain.cs
@@ -0,0 +1,55 @@
+using AGS.Types;
+using System;
+
+
+namespace AGS.Plugin.AgsGet
+{
+
+    [RequiredAGSVersion("3.4.3.1")]
+    public class PluginMain : AGS.Types.IAGSEditorPlugin
+    {
+        private IAGSEditor _editor;
+
+        public PluginMain(IAGSEditor editor)
+        {
+            _editor = editor;
+        }
+
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~PluginMain()
+        // {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ agsget/agsget/EditorPlugin/README.md
@@ -0,0 +1,7 @@
+# AgsGet Editor Plugin
+
+Before starting, install AGS 3.5.0 to the regular directory:
+
+```
+choco install ags --version 3.5.0.23
+```
